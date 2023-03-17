7aa0d5419c6db4b4a1b98a523d6e281ee4662485
RasitCelebi <rasit_61@hotmail.com>
2023-03-17 16:44:41 +0300

    x

--- src/simple.js
+++ src/simple.js
@@ -3,6 +3,9 @@ let clickEvent = document.querySelector('.myMenuUl');
 
 clickEvent.addEventListener("click", function (event) {
     console.log("number of Children : " + event.target.childNodes.length + " child");
+    console.log("target : " + event.target);
+    console.log("target : " + event.target.childElementNodes);
+    
 
     if (event.target.childNodes.length > 1) {
         event.target.querySelector('ul').classList.toggle("hidden");
@@ -20,6 +23,12 @@ clickEventSub.forEach(item =>
             event.target.parentNode.classList.toggle("block");
         }
     ));
+
+
+    let x = [0,1,2,3,4,5,6];
+console.log(x);
+    x.splice(3,2,10,11);
+    console.log(x);
 
 
 
